48260f6c86dc8023640cdf85b04e33efa15195d1
Marcos Aurelio Oripka <aureliomarkos@yahoo.com>
2026-02-08 12:24:10 -0300

    configuração do arquivo .env

--- app/core/database.py
+++ app/core/database.py
@@ -2,10 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
+
+
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {},
+    echo=settings.DEBUG
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
